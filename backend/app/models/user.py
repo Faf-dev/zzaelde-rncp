635aff1803fed26c9bae79e7d471d75bf2268175
Faf-dev <noaillesjulien@gmail.com>
2026-07-30 02:27:29 +0200

    ajout : id en primary key + contrainte de création d'un mot de passe fort

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,12 +1,32 @@
 from app import db
+import uuid
+import re
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.orm import validates
 
 
 class User(db.Model):
     __tablename__ = "user"
 
-    username = db.Column(db.String(256), unique=True, nullable=False, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    username = db.Column(db.String(256), unique=True, nullable=False)
     mot_de_passe = db.Column(db.String(256), nullable=False)
+
+    @validates('mot_de_passe')
+    def validatePassword(self, key, value):
+        if not isinstance(value, str):
+            raise TypeError("Le mot de passe doit être une chaîne de caractères")
+        if len(value) < 8:
+            raise ValueError("Le mot de passe doit comporter au moins 8 caractères")
+        if not re.search(r"[A-Z]", value):
+            raise ValueError("Le mot de passe doit contenir au moins une majuscule")
+        if not re.search(r"[a-z]", value):
+            raise ValueError("Le mot de passe doit contenir au moins une minuscule")
+        if not re.search(r"[0-9]", value):
+            raise ValueError("Le mot de passe doit contenir au moins un chiffre")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
+            raise ValueError("Le mot de passe doit contenir au moins un caractère spécial")
+        return self.hash_password(value)
 
     @staticmethod
     def hash_password(password):
